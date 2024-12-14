e24518062072d422ddff0851aebd9139981d0a2c
晨魔 <139463472+MorningDemon515@users.noreply.github.com>
2024-12-14 13:44:00 +0800

    Fast Inverse Square Root

--- Hello.cs
+++ Hello.cs
@@ -1,17 +1,20 @@
-using System;
+using q_math;
 using System.Runtime.InteropServices;
 
 namespace Hello
 {
     class HelloClass
     {
-        [DllImport("add.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        private static extern int Sum();
+        //[DllImport("add.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        //private static extern int Sum();
+
+        q_mathclass qc = new q_mathclass();
 
         public void HelloWorld()
         {
-            
-           Sum();
+            float y = qc.Q_rsqrt(2.0F);
+          // Sum();
+          Console.WriteLine(y);
         }
     }
 }
